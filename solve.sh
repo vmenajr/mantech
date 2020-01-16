d5b09e025917766fde337cc0f194812fd01cba7c
Vick Mena <vickmena@gmail.com>
2020-01-16 08:11:28 -0500

    Dump out the bin as well

--- solve.sh
+++ solve.sh
@@ -1,11 +1,17 @@
 #!/usr/bin/env bash
+# https://www.mantech.com/mantech-cyber-challenge
+#
+url_bin1=https://www.mantech.com/sites/default/files/2019-03/Algo1_algorithm.txt
+url_msg1=https://www.mantech.com/sites/default/files/2019-03/Algo1_message.txt
+url_bin2=https://www.mantech.com/sites/default/files/2019-03/Algo2_algorithm.txt
+url_msg2=https://www.mantech.com/sites/default/files/2019-03/Algo2_message.txt
 
 function print_flag() {
     konstant=$1
     url=$2
     bytes=$(od -An -w1 -t d1 -v < <(curl -s -L ${url}))
     flag=''
-    echo k=${konstant}
+    echo -n k=${konstant},\ 
     while read byte; do
         d=$((${byte} ^ ${konstant}))
         #echo ${byte} '=>' ${d}
@@ -14,21 +20,13 @@ function print_flag() {
     echo ${flag}
 }
 
-print_flag $((0xa4 ^ 0xae ^ 0x9c ^ 0x6f)) https://www.mantech.com/sites/default/files/2019-03/Algo1_message.txt
-print_flag $((0x54 ^ 0x6e ^ 0x40 ^ 0x4d ^ 0x43 ^ 0x68 ^ 0x63 ^ 0x72 ^ 0x62 ^ 0x79 ^ 0x21)) https://www.mantech.com/sites/default/files/2019-03/Algo2_message.txt
-exit
+curl -s --output /tmp/asm.bin ${url_bin1}; /usr/local/Cellar/binutils/2.32/bin/objdump -D -Mintel,x86-64 -m i386 -b binary /tmp/asm.bin
+echo
+curl -s --output /tmp/asm.bin ${url_bin2}; /usr/local/Cellar/binutils/2.32/bin/objdump -D -Mintel,x86-64 -m i386 -b binary /tmp/asm.bin
+echo
+echo
+echo
 
-flag=''
-#v=$((0x54 ^ 0x6e ^ 0x40 ^ 0x4d ^ 0x43 ^ 0x68 ^ 0x63 ^ 0x72 ^ 0x62 ^ 0x79 ^ 0x21))
-v=$((0xa4 ^ 0xae ^ 0x9c ^ 0x6f))
-echo v=${v}
-#bytes=$(od -An -w1 -t d1 -v < <(curl -L https://www.mantech.com/sites/default/files/2019-03/Algo2_message.txt))
-bytes=$(od -An -w1 -t d1 -v < <(curl -L https://www.mantech.com/sites/default/files/2019-03/Algo1_message.txt))
-while read byte; do
-    d=$((${byte} ^ ${v}))
-	echo ${byte} '=>' ${d}
-    flag="${flag}$(echo ${d} | awk '{printf("%c",$1)}')"
-done < <(printf "%s" "$bytes")
-echo ${flag}
-exit
+print_flag $((0xa4 ^ 0xae ^ 0x9c ^ 0x6f)) ${url_msg1}
+print_flag $((0x54 ^ 0x6e ^ 0x40 ^ 0x4d ^ 0x43 ^ 0x68 ^ 0x63 ^ 0x72 ^ 0x62 ^ 0x79 ^ 0x21)) ${url_msg2}
 
